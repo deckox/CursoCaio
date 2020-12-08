ba3937eb9dba8c35cbd9fab219bc7de24d1df54c
Andre Kimura <akimura@rdisoftware.com>
2020-12-08 14:11:03 -0300

    ultimo exercicio de data incluido

--- 4/Program.cs
+++ 4/Program.cs
@@ -22,20 +22,17 @@ namespace _4
             var dateDDMMAA = dateAnyFormat.ToString("dd/MM/yyyy");
             var datePlusOneDay = dateAnyFormat.AddDays(1);
             var dateLessFourHours = dateAnyFormat.AddHours(-4);
-            //var date = "20201021T172410";
 
-
-            CultureInfo culture2 = new CultureInfo("en-US");
-           // string myDate2 = "20201021T172410";
-            var myDate2 = "20201021172410";
-           
-
+            var dateString = "20201021T172410";
+            DateTime myDate = DateTime.ParseExact(dateString, "yyyyMMddTHHmmss",
+                                        System.Globalization.CultureInfo.InvariantCulture);
 
             Console.WriteLine(dateAnyFormat);
             Console.WriteLine(dateDDMMAA);
             Console.WriteLine(datePlusOneDay);
             Console.WriteLine(dateLessFourHours);
-            
+            Console.WriteLine(myDate);
+
         }
     }
 }
